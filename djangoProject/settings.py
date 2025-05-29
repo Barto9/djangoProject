0cb735ea88c13805155e27f278dce82a3cf88d8b
Barto9 <bartekgawryszuk@gmail.com>
2025-05-29 19:03:22 +0200

    database connection init

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
