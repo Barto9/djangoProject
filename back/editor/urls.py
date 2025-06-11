e2d2a7fd97351da83b93b7fb7a115684d67b6d6c
StudentSmerf <272976@student.pwr.edu.pl>
2025-06-11 17:11:48 +0200

    naprawienie menu

--- back/editor/urls.py
+++ back/editor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import NoteListCreateView, NoteDetailView, NoteHierarchyView, GDDListCreateView
 
 urlpatterns = [
-    path('editor/gdds/', GDDListCreateView.as_view(), name='gdd-list-create'),
+    path('gdds/', GDDListCreateView.as_view(), name='gdd-list-create'),
     path('gdd/<int:gdd_id>/notes/', NoteListCreateView.as_view(), name='note-list-create'),
     path('gdd/<int:gdd_id>/notes/<int:pk>/', NoteDetailView.as_view(), name='note-detail'),
     path('gdd/<int:gdd_id>/notes-hierarchy/', NoteHierarchyView.as_view(), name='note-hierarchy'),
