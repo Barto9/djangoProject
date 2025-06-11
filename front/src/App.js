b455fdab58b911183f34dadc4a951bfb273e5e57
Sedna <seventesprit@gmail.com>
2025-06-11 12:19:08 +0200

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import LoginScreen from "./components/LoginScreen";
 import GDDList from "./components/GddList";
-import NoteList from "./components/NoteList";
 import GDDCreator from './components/GDDCreator';
 import NoteScreen from "./components/NoteScreen";
 
